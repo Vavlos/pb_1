8b574a133873ea3f61784622947f26e49a945490
Pavel Novikov <novikovpav2015@yandex.ru>
2024-12-16 10:53:38 +0300

    l31

--- src/main.cpp
+++ src/main.cpp
@@ -79,7 +79,7 @@ void GrapAndPut(int& beeper_counter){
 /// Стартовая функция
 ///
 int main() {
-    LoadWorld("worlds/Double_beepers_01.w");
+    LoadWorld("worlds/Double_beepers_02.w");
     KarelProgram();
     Finish();
     return 0;
